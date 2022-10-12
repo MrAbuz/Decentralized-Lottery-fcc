e074239e204227b6e1e304b1117c82304425f653
MrAbuz <miguelmrosario@gmail.com>
2022-10-12 22:36:18 +0100

    seventh commit

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,11 +1,41 @@
 const { developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+
+const BASE_FEE = ethers.utils.parseEther("0.25")
+//this means that for each request there's a base fee of 0.25 link (premium) (*)
+//we normaly don't have to assign this because in normal chains this is a deployed contract where we obviously dont pass any arguments for constructor and we just call
+// its functions, but here we do assign some variables cuz its a mock and those values are variable.
+//the reason this costs 0.25 link per request and the price feeds didnt cost anything is because the price feeds are sponsored by a lot of protocols that are paying for those
+//requests already, whereas here we are the only ones asking for this request.
+//nice how we use parseEther even tho its 0.25 link and not eth.
+
+const GAS_PRICE_LINK = 1e9 //we setted this to a price Patrick said, kind of random
+//this number normaly fluctuates, but here we must assign a value cuz its a mock
+//calculated value based on the gas price of the chain
+//since the chainlink nodes are the ones making calls to our functions, they have to have a way to measure the gas of the chain so they don't go bankrupt.
+//the more the price of gas goes up on the chain we using, the highest the price of the request we pay to the chainlink nodes
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks Deployed!")
+        log("--------------------------")
     }
 }
+
+module.exports.tags = ["all", "mocks"]
+
+// We have this "mocks" folder in the chainlink github with some mocks:
+// https://github.com/smartcontractkit/chainlink/tree/develop/contracts/src/v0.8/mocks
+
+// (*) We can find the premium, or FLAT FEE at:
+// https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,7 +1,24 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
+    //remember: this was created by us to query different variables for each chain
     5: {
         name: "goerli",
         vrfCoordinatorV2: "0x2ca8e0c643bde4c2e08ab1fa0da3401adad7734d", // *
+        entranceFee: ethers.utils.parseEther("0.01"),
+        //we want to make the entrance fee different depending on the chain we're on; if we're on a more expensive chain we wanna make this higher than others
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: "0",
+        callbackGasLimit: "500000", //this also does vary from network to network, so we'll add it here; "pretty high limit of 500 thousand gas"
+        interval: "30", //the lottery restarts after 30s
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", //we're using the same one even tho its irrelevant, just to fill, it doesnt matter
+        //which gas lane we input since here it'll be on hardhat/localhost
+        callbackGasLimit: "500000",
+        interval: "30",
     },
 }
 
@@ -11,4 +28,5 @@ module.exports = {
     networkConfig,
     developmentChains,
 }
-// * veio de: https://docs.chain.link/docs/vrf/v2/direct-funding/supported-networks/
+
+// * e ** vieram de : https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/
